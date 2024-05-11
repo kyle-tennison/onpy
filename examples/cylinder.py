9d0eeba12b7a60f9db1eaddbe8e761e97d47ad5e
Kyle Tennison <kyletennison05@gmail.com>
2024-05-10 22:57:17 -0700

    fix: update example syntax

--- examples/cylinder.py
+++ examples/cylinder.py
@@ -14,22 +14,16 @@ document = client.create_document("Cylinder Example")
 partstudio = document.get_partstudio()
 
 # now, we'll define a sketch
-sketch = Sketch(
-    partstudio=partstudio,  # we pass a reference to the partstudio
+sketch = partstudio.add_sketch(
     plane=partstudio.features.top_plane,  # we define the plane to draw on
     name="New Sketch",  # and we name the sketch
 )
 
 # in this new sketch, we'll draw a circle
-sketch.add_circle(center=(0, 0), radius=0.5)  # the default units are inches, 🇺🇸
-
-# that's all we'll add to this sketch. we can add this to the partstudio with:
-partstudio.add_feature(sketch)
+sketch.add_circle(center=(0, 0), radius=0.5)  # the default units are inches
 
 # next, we'll extrude the sketch. the syntax is similar
-extrude = Extrude(
-    partstudio=partstudio,  # another partstudio reference
-    targets=[sketch],  # we'll extrude the entire sketch ...
+extrude = partstudio.add_extrude(
+    faces=sketch,  # we'll extrude the entire sketch ...
     distance=1,  # ... by one inch
 )
-partstudio.add_feature(extrude)
