915a2b8caf1fbe85f8352d0c01df68ece0a8863e
Kyle Tennison <135751865+kyle-tennison@users.noreply.github.com>
2024-10-07 09:11:46 -0700

    fix ci (#48)
    * ci: re-enable examples test
    
    * ci: add mypy dep
    
    * ci: add library stubs for requests
    
    * tests: update kwarg name
    
    * ci: add nbconvert dep
    
    * ci: relay environ
    
    * lint

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -20,7 +20,7 @@ def test_lifecycle():
     doc_copy1 = client.get_document(name=document_name)
     assert doc_copy1 == document
 
-    doc_copy2 = client.get_document(id=document.id)
+    doc_copy2 = client.get_document(document_id=document.id)
     assert doc_copy2 == document
 
     new_document = client.create_document(document_name + "_modified")
@@ -34,13 +34,13 @@ def test_exceptions():
     """Tests exertions relating to documents"""
 
     with pytest.raises(OnPyParameterError) as _:
-        client.get_document(id="not a id")
+        client.get_document(document_id="not a id")
 
     with pytest.raises(OnPyParameterError) as _:
         client.get_document()
 
     with pytest.raises(OnPyParameterError) as _:
-        client.get_document(id="123", name="123")
+        client.get_document(document_id="123", name="123")
 
 
 def test_elements():
@@ -51,7 +51,7 @@ def test_elements():
     try:
         main_ps = document.get_partstudio(name="Part Studio 1")
 
-        assert main_ps == document.get_partstudio(id=main_ps.id)
+        assert main_ps == document.get_partstudio(element_id=main_ps.id)
         assert main_ps in document.elements
     finally:
         document.delete()
